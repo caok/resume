594b90ac4a72f4644b6a0571a9fe1259a1371226
caok <caok1231@gmail.com>
2013-06-26 13:05:23 +0800

    modify the show of timeline

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   attr_accessible :created_at, :introduction, :name, :finished_at, :url, :user_id
+
+  def happened_at
+    created_at.to_s + (finished_at.blank? ? '' : ('-----' + finished_at.to_s))
+  end
 end
